8d41d727689209364b2babb6277147848a19b25e
Yasser <yasser.hamdan@montyholding.com>
2024-09-19 10:40:06 -0400

    Initial Commit

---
+++ Rides/RidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RidesApp.swift
+//  Rides
+//
+//  Created by yasser hamdan on 19/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct RidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
